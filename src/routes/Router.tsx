a14cb7946d3f8e9db21637606ecf92e9233048e2
thomascostta <thomas.jeffcosta@gmail.com>
2022-06-20 10:58:10 -0300

    create Router

---
+++ src/routes/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppStack } from './AppStack';
+import { AuthStack } from './AuthStack';
+
+export function Router() {
+    const auth = true;
+
+    return (
+        <NavigationContainer >
+            {auth ? <AppStack /> : <AuthStack />}
+        </NavigationContainer>
+    )
+}
