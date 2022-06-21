7855ae499ea0c8515b785b1009376bd8fe6291f3
thomascostta <thomas.jeffcosta@gmail.com>
2022-06-21 14:40:23 -0300

    settings

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppStack } from './AppStack';
 import { AuthStack } from './AuthStack';
+import { useAuth } from '../contexts/Auth';
+import { Text, View } from 'react-native';
 
 export function Router() {
-    const auth = true;
+    const { authData, isLoading } = useAuth();
+
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Carregando aplicativo...</Text>
+            </View>
+        );
+    }
 
     return (
         <NavigationContainer >
-            {auth ? <AppStack /> : <AuthStack />}
+            {authData ? <AppStack /> : <AuthStack />}
         </NavigationContainer>
     )
 }
