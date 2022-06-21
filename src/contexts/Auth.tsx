33696f8d5fe4d5ca62ebc351f0ab826ac1cd8fae
thomascostta <thomas.jeffcosta@gmail.com>
2022-06-21 14:39:43 -0300

    auth

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,55 +1,76 @@
-import React, { useContext } from 'react'
-import { createContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { Alert } from 'react-native';
-import { authService } from "../services/authService";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { authService } from '../service/authService';
 
 export interface AuthData {
     token: string;
     email: string;
     name: string;
 }
+
 interface AuthContextData {
     authData?: AuthData;
-    signIn: (email: string, password: string) => Promise<AuthData>;
+    signIn: (email: string, password: string) => Promise<void>;
     signOut: () => Promise<void>;
+    isLoading: boolean;
 }
 
-export const AuthContext = createContext<AuthContextData>(
-    {} as AuthContextData
-);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
     const [authData, setAuthData] = useState<AuthData>();
+    const [isLoading, setisLoading] = useState(true);
 
-    async function signIn(email: string, password: string): Promise<AuthData> {
+    useEffect(() => {
+        loadStorageData();
+    }, []);
+
+    async function loadStorageData(): Promise<void> {
         try {
-            const auth = await authService.signIn(email, password);
-            setAuthData(auth);
+            const authDataSerialized = await AsyncStorage.getItem('@AuthData');
 
-            return auth;
+            if (authDataSerialized) {
+                const _authData: AuthData = JSON.parse(authDataSerialized);
+                setAuthData(_authData);
+            }
 
         } catch (error) {
-            Alert.alert(error.message, 'Usuário e/ou senha inválidos. Tente novamente...')
+        } finally {
+            setisLoading(false);
         }
-    }
+    };
 
-    async function signOut(): Promise<void> {
+    async function signIn(email: string, password: string) {
+        try {
+            const authData = await authService.signIn(email, password);
+
+            setAuthData(authData);
+            AsyncStorage.setItem('@AuthData', JSON.stringify(authData));
+        } catch (error: any) {
+            Alert.alert(error.message, 'Tente novamente');
+        }
+    };
+
+    async function signOut() {
         setAuthData(undefined);
-
-        return;
-    }
+        AsyncStorage.removeItem('@AuthData');
+    };
 
     return (
-        <AuthContext.Provider
-            value={{
-                authData, signIn, signOut
-            }}>
+        <AuthContext.Provider value={{ authData, signIn, signOut, isLoading }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
-export function useAuth() {
+export function useAuth(): AuthContextData {
     const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+
     return context;
 }
