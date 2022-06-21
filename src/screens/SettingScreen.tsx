6639fcdf4b113d84e24a029d990bfcfd649f1df8
thomascostta <thomas.jeffcosta@gmail.com>
2022-06-21 14:40:49 -0300

    settings

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { MyButton } from '../components/MyButtons';
+import { useAuth } from '../contexts/Auth';
 
 import { styles } from './styles';
 
 export function SettingScreen() {
+    const { signOut } = useAuth();
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Configurações</Text>
-            <MyButton style={{ backgroundColor: 'red' }} title='Sair do App' />
+            <MyButton
+                style={{ backgroundColor: 'red' }}
+                title='Sair do App'
+                onPress={signOut}
+            />
         </View>
     );
 }
